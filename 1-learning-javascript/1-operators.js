0ab60e8f9598c68696a8d41064757f7babf8b6d5
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-25 12:59:11 +0530

    updates

---
+++ 1-learning-javascript/1-operators.js
@@ -0,0 +1,29 @@
+/*** Operator ***/
+
+// add/concatenation    Used to add two numbers together, or glue two strings together.
+6 + 9;
+"Hello " + "world!";
+
+// subtract, multiple, divide  These do what you'd expect them to do in basic math.
+2 - 3;
+8 * 2; // multiply in JS is an asterisk
+9 / 3; // divide is forwad slash
+
+// assignment operator You've seen this already: it assigns a value to a variable.
+var myVariable = 'Bob';
+
+// Identity Operator    Does a test to see if two values are equal to one another
+// and returns a true/false (boolean) result.
+
+// Here we check if 4 EQUALS 4, which is true
+var myVariable = 4;
+myVariable === 4; // true
+
+// Here we check if 3 EQUALS 4, which is false
+var myVariable = 3;
+myVariable === 4; // falses
+
+// Here we check if 3 NOT equals 3 which is false
+var myVariable = 3;
+myVariable !== 3;
+false
