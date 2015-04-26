899434980a1bfaf3853bfd258da6783b0f94a511
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-26 12:18:02 +0530

    updates

--- 2-node-basics/hello-world.js
+++ 2-node-basics/hello-world.js
@@ -1,1 +1,26 @@
 console.log('hello world');
+
+// https://nodejs.org/docs/latest/api/process.html#process_process_argv
+var argv = {};
+
+// The arguments are stored in process.argv
+process.argv.forEach(function (val, index) {
+    var keyValue;
+    if (index < 2) {
+        return;
+    }
+    keyValue = val.split(':');
+    argv[keyValue[0]] = keyValue[1];
+});
+
+console.log(argv);
+
+
+process.on('exit', function (code) {
+    setTimeout(function () {
+        console.log('This will not run');
+    }, 0);
+    console.log('Exiting with code: ', code);
+});
+
+process.exit(0);
