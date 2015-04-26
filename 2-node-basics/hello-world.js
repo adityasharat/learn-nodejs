d59432d4050e131457d883c4c36767806075c04d
Aditya Sharat <aditya.sharat@flipkart.com>
2015-04-26 12:21:39 +0530

    updates

--- 2-node-basics/hello-world.js
+++ 2-node-basics/hello-world.js
@@ -33,3 +33,19 @@ process.on('exit', function (code) {
 process.exit(0);
 
 */
+
+/*
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
+
+setTimeout(function() {
+  console.log('This will still run.');
+}, 500);
+
+// Intentionally cause an exception, but don't catch it.
+noSuchFunction();
+console.log('This will not run.');
+
+*/
