8c390b18697ba096aec36fbea93fe69b1dece564
Aditya Sharat <aditya.sharat@flipkart.com>
2015-05-05 18:35:40 +0530

    updates

--- 2-node-basics/1-timers.js
+++ 2-node-basics/1-timers.js
@@ -1,1 +1,24 @@
 /*** Timers ***/
+
+setTimeout(function () {
+    console.log('this will run after 2 seconds');
+}, 2000);
+
+var handle = setTimeout(function () {
+    console.log('this will not run after 5 seconds');
+}, 5000);
+
+setTimeout(function () {
+    console.log('clear the timeout.');
+    // this is used to clear a timeout
+    clearTimeout(handle);
+}, 3000);
+
+var anotherHandle = setInterval(function () {
+    console.log('2 seconds have passed.');
+}, 2000);
+
+setTimeout(function () {
+    console.log('stop the set interval.');
+    clearInterval(anotherHandle);
+}, 10000);
