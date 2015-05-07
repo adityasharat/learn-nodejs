16e49a7f34511006a7b084418b34ac8752a274fd
Aditya Sharat <aditya.sharat@flipkart.com>
2015-05-07 16:55:24 +0530

    updates

--- 2-node-basics/3-event-emitters.js
+++ 2-node-basics/3-event-emitters.js
@@ -6,6 +6,10 @@ phone.on('incomingCall', function (contact) {
     console.log(contact + ' calling.');
 });
 
-setInterval(function () {
+var handle = setInterval(function () {
     phone.emit('incomingCall', 'Darth Vader');
 }, 1000);
+
+setTimeout(function () {
+    clearInterval(handle);
+}, 5500);
