82289b11ecc3e4926beb55701b3e0e1052a8e1d4
Aditya Sharat <aditya.sharat@flipkart.com>
2015-05-11 20:13:19 +0530

    updates

--- 2-node-basics/5-file-system.js
+++ 2-node-basics/5-file-system.js
@@ -1,2 +1,15 @@
 /*** File System ***/
 var fs = require('fs');
+var filename = 'data/cuboid-0.json';
+var options = {
+    //encoding: 'utf8'
+};
+
+fs.readFile(filename, options, function (err, data) {
+    if (err) {
+        throw err;
+    }
+    console.log(data);
+    /*var cuboid0 = JSON.parse(data);
+    console.log(cuboid0);*/
+});
